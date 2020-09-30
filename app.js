bc75a9ee778a419a7a58375e89f030f9e8ddd642
root <root@Matthew.localdomain>
2020-09-30 11:56:46 -0400

    fixed app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index");
 
 //mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connect("mongodb+srv://matt:kingmatthew6@cluster0.k3evd.mongodb.net/yelpcamp?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://matt:kingmatthew6@cluster0.k3evd.mongodb.net/yelpcamp?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
